7b8a79211ded0b14bf6a2f745888865fb02a9860
Ben Rogerson <info@benrogerson.dev>
2024-03-12 19:16:43 +1030

    Add favicons

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -14,8 +14,8 @@ const Introduction = () => {
   return (
     <div className="pointer-events-none fixed z-40 h-[100dvh] w-[100dvw] overflow-hidden">
       <div className="delay-2000 absolute grid h-full w-full place-content-center bg-background duration-1000 animate-out fade-out fill-mode-forwards">
-        <div className="-ml-4 -mt-6 flex items-center text-2xl delay-0 duration-1000 ease-out animate-in zoom-in-110 fill-mode-backwards sm:text-4xl">
-          <IconDrip className="-mr-1 -mt-2 text-[175%]" />
+        <div className="-ml-4 -mt-6 flex items-center gap-1 text-2xl delay-0 duration-1000 ease-out animate-in zoom-in-110 fill-mode-backwards sm:text-4xl">
+          <IconDrip className="-mt-4 text-[175%]" />
           <div>DripWave</div>
         </div>
       </div>
